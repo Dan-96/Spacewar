1d5517128c126843e588eb095abcd15594f46d02
Dan-96 <daniel.michalczyk96@gmail.com>
2024-01-22 23:44:39 +0000

    Alpha-v0.1.3
    -Added an option to change resolution in options menu
    
    -Added ship to ship collisions
    
    -Added ship to black hole collisions
    
    -Adjusted the explosion to better match ship's position
    
    -Changed the default resolution from 1900x1000 to 1600x900 to match 16:9 aspect ratio
    
    -Fixed a bug where sometimes the game crashed when a bullet went exactly through the middle of the screen

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame
-
 import math
-
 import random
-
+import configparser
 
 
 class Player:
@@ -28,7 +26,7 @@ class Player:
         self.rotated_rect = self.rotated_surf.get_rect(center=(self.x, self.y))
         self.velocity_x = 0
         self.velocity_y = 0
-        self.gravity_point = pygame.math.Vector2(WIDTH // 2, HEIGHT // 2)
+        self.gravity_point = pygame.math.Vector2(width // 2, height // 2)
         self.gravitational_force = 0.02
         self.current_sprite = 0
         self.bullet_count = 1
@@ -61,6 +59,13 @@ class Player:
         self.sine = math.sin(math.radians(self.angle + 90))
         self.angle_velocity *= 0.96
 
+    def check_ship_collision(self, object):
+        distance = math.sqrt((self.x - object.x) ** 2 + (self.y - object.y) ** 2)
+        if distance < object.w:
+            self.health -= 1
+            pygame.time.set_timer(restart_event, 3000, 1)
+            pygame.mixer.Channel(3).play(player1.sound_explode)
+
     def apply_thrust(self):
         self.thrust_force = 0.02 # Default: 0.02
         self.velocity_x += self.cosine * self.thrust_force
@@ -75,7 +80,7 @@ class Player:
     def update_location(self):
         self.x += self.velocity_x
         self.y += self.velocity_y
-        if self.x >= WIDTH or self.x <= 0 or self.y <= 0 or self.y >= HEIGHT:
+        if self.x >= width or self.x <= 0 or self.y <= 0 or self.y >= height:
             self.health -= 1000
             pygame.time.set_timer(restart_event, 3000, 1)
             pygame.mixer.Channel(3).play(player1.sound_explode)
@@ -108,7 +113,7 @@ class Bullet(Player):
         else:
             self.velocity_x = 0
             self.velocity_y = 0
-        self.gravity_point = pygame.math.Vector2(WIDTH // 2, HEIGHT // 2)
+        self.gravity_point = pygame.math.Vector2(width // 2, height // 2)
         self.gravitational_force = 0 # How much bullets are attracted towards centre of the screen
 
     def update_bullet(self):
@@ -116,11 +121,11 @@ class Bullet(Player):
         self.y += self.velocity_y
         self.apply_bullet_vector()
 
-    def check_bullet_collisions(self, player):
+    def modify_bullet_list(self, player):
         for i in self.bullet_list:
             i.draw_bullet(screen)
             i.update_bullet()
-            if i.x < 0 or i.x > WIDTH or i.y < 0 or i.y > HEIGHT:
+            if i.x < 0 or i.x > width or i.y < 0 or i.y > height:
                 self.bullet_list.remove(i)
             if player.health >= 1:
                 if i.check_ship_collision(player.rotated_rect):
@@ -134,7 +139,10 @@ class Bullet(Player):
     def apply_bullet_vector(self):
         # If enabled calculates bullet gravity towards the centre
         self.gravity_direction = self.gravity_point - pygame.math.Vector2(self.x, self.y)
-        self.gravity_direction.normalize_ip()
+        try:
+            self.gravity_direction.normalize_ip()
+        except ValueError:
+            pass
         self.velocity_x += self.gravity_direction.x * self.gravitational_force
         self.velocity_y += self.gravity_direction.y * self.gravitational_force
 
@@ -182,8 +190,8 @@ def bullets_collide():
 def restart_game():
     # Player 1 reset settings
     player1.angle = 90
-    player1.x = 1266
-    player1.y = 500
+    player1.x = (width // 1.5)
+    player1.y = (height // 2.001)
     player1.health = 1
     # Shared settings to reset
     player1.velocity_x, player2.velocity_x = 0, 0
@@ -193,8 +201,8 @@ def restart_game():
     player1.angle_velocity, player2.angle_velocity = 0, 0
     # Player 2 reset settings
     player2.angle = 270
-    player2.x = 633
-    player2.y = 500
+    player2.x = (width // 3)
+    player2.y = (height // 2.001)
     player2.health = 1
     # Clear all bullets
     player1_bullet.bullet_list.clear()
@@ -204,6 +212,20 @@ def restart_game():
     powerups['shield']['collected'] = True
     pygame.time.set_timer(ammo_event, 3000, 1)
     pygame.time.set_timer(shield_event, 15000, 1)
+
+
+def black_hole(player):
+    pygame.draw.circle(screen, 'white', (width // 2, height // 2), 62)
+    black_hole = pygame.draw.circle(screen, 'black', (width // 2, height // 2), 60)
+    black_hole_pos = black_hole.center
+    black_hole_radius = black_hole.width // 2
+    if player.health >= 1:
+        distance = math.sqrt((player.x - black_hole_pos[0]) ** 2 + (player.y - black_hole_pos[1]) ** 2)
+        if distance < player.h / 2 + black_hole_radius:
+            player.health -= 1
+            pygame.time.set_timer(restart_event, 3000, 1)
+            pygame.mixer.Channel(3).play(player1.sound_explode)
+            player.explode()
 
 
 def display_text(text, x, y, colour='gray33', size=30):
@@ -220,67 +242,100 @@ def check_powerup(player):
             if powerup_name == 'ammo':
                 pygame.draw.circle(screen, powerup_data["color"], (ammo_x, ammo_y), 8)
                 distance = math.sqrt((player.x - ammo_x) ** 2 + (player.y - ammo_y) ** 2)
-                if distance < player.w / 2 + 8:
+                if distance < player.h - 5:
                     player.bullet_count += 1
                     powerup_data["collected"] = True
                     pygame.time.set_timer(ammo_event, 3000, 1)
             if powerup_name == 'shield':
                 pygame.draw.circle(screen, powerup_data["color"], (shield_x, shield_y), 8)
                 distance = math.sqrt((player.x - shield_x) ** 2 + (player.y - shield_y) ** 2)
-                if distance < player.w / 2 + 8:
+                if distance < player.h - 5:
                     player.health += 1
                     powerup_data["collected"] = True
                     pygame.time.set_timer(shield_event, 15000, 1)
                     pygame.time.set_timer(shield_end_event, 10000, 1)
 
 
+def resolution_changer():
+    config = configparser.ConfigParser()
+    config_path = 'Config/Config.ini'
+    config.read(config_path)
+    def set_res(width, height):
+        config['Resolution']['width'] = str(width)
+        config['Resolution']['height'] = str(height)
+    def save_config():
+        with open(config_path, 'w') as config_file:
+            config.write(config_file)
+    screen_width = int(config.get('Resolution', 'width'))
+    screen_height = int(config.get('Resolution', 'height'))
+    return screen_width, screen_height, set_res, save_config
+
+
 def menu_screen():
     screen.fill('black')
     logo = pygame.image.load('Graphics/logo.png').convert_alpha()
-    resized_logo = pygame.transform.scale(logo, (742, 116))
-    screen.blit(resized_logo, (579, 100))
-    display_text('alpha-v0.1.2', 1400, 210, 'grey', 20)
-    display_text('START', 950, 450)
-    display_text('OPTIONS', 950, 500)
-    display_text('EXIT', 950, 550)
+    resized_logo = pygame.transform.scale(logo, ((width // 2.56), (height // 8.62)))
+    screen.blit(resized_logo, ((width // 3.28), (height // 10)))
+    display_text('alpha-v0.1.3', (width // 1.36), (height // 4.76), 'grey', (width // 95))
+    display_text('START', w_half, h_half - 50)
+    display_text('OPTIONS', w_half, h_half)
+    display_text('EXIT', w_half, h_half + 50)
     if menu_selection == 0:
-        display_text('> START <', 950, 448, 'white')
+        display_text('> START <', w_half, h_half - 52, 'white')
     elif menu_selection == 1:
-        display_text('> OPTIONS <', 950, 498, 'white')
+        display_text('> OPTIONS <', w_half, h_half - 2, 'white')
     elif menu_selection == 2:
-        display_text('> EXIT <', 950, 548, 'white')
+        display_text('> EXIT <', w_half, h_half + 48, 'white')
 
 
 def options_screen():
     screen.fill('black')
-    display_text('RESOLUTION (SOON)', 950, 450)
-    display_text('CONTROLS (SOON)', 950, 500)
-    display_text('SAVE & EXIT', 950, 550)
-    if options_selection == 0:
-        display_text('> RESOLUTION (SOON) <', 950, 448, 'white')
-    elif options_selection == 1:
-        display_text('> CONTROLS (SOON) <', 950, 498, 'white')
-    elif options_selection == 2:
-        display_text('> SAVE & EXIT <', 950, 548, 'white')
+    display_text('RESOLUTION', w_half, h_half - 50)
+    display_text('CONTROLS (SOON)', w_half, h_half)
+    display_text('EXIT', w_half, h_half + 50)
+    if menu_selection == 0:
+        display_text('> RESOLUTION <', w_half, h_half - 52, 'white')
+    elif menu_selection == 1:
+        display_text('> CONTROLS (SOON) <', w_half, h_half - 2, 'white')
+    elif menu_selection == 2:
+        display_text('> EXIT <', w_half, h_half + 48, 'white')
+
+
+def resolution_screen():
+    screen.fill('black')
+    display_text(f'CURRENT RESOLUTION: {width} x {height}', w_half, h_half - 100)
+    display_text('1600 x 900', w_half, h_half - 50)
+    display_text('1280 x 720', w_half, h_half)
+    display_text('854 x 480', w_half, h_half + 50)
+    display_text('SAVE & EXIT', w_half, h_half + 100)
+    if menu_selection == 0:
+        display_text('> 1600 x 900 <', w_half, h_half - 52, 'white')
+    elif menu_selection == 1:
+        display_text('> 1280 x 720 <', w_half, h_half - 2, 'white')
+    elif menu_selection == 2:
+        display_text('> 854 x 480 <', w_half, h_half + 48, 'white')
+    elif menu_selection == 3:
+        display_text('> SAVE & EXIT <', w_half, h_half + 98, 'white')
 
 
 pygame.init()
-WIDTH = 1900
-HEIGHT = 1000
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.transform.scale(screen, (800, 600))
+width, height = resolution_changer()[:2]
+w_half = (width // 2)
+h_half = (height // 2)
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Spacewar")
 icon = pygame.image.load('Graphics/Player_1_Thrust.png')
+# background = pygame.image.load('Graphics/Background.png').convert_alpha()
 pygame.display.set_icon(icon)
 mouse_x, mouse_y = pygame.mouse.get_pos()
-ammo_x, ammo_y = random.randint(10, 1890), random.randint(10, 990)
-shield_x, shield_y = random.randint(10, 1890), random.randint(10, 990)
-triple_x, triple_y = random.randint(10, 1890), random.randint(10, 990)
-player1 = Player(1266, 500, 90, 'Player_1')
-player2 = Player(633, 500, 270, 'Player_2')
+ammo_x, ammo_y = random.randint(20, (width - 20)), random.randint(20, (height - 20))
+shield_x, shield_y = random.randint(20, (width - 20)), random.randint(20, (height - 20))
+player1 = Player((width // 1.5), (height // 2), 90, 'Player_1')
+player2 = Player((width // 3), (height // 2), 270, 'Player_2')
 player1_bullet = Bullet(player1.x, player1.y, player1.angle, True, player1)
 player2_bullet = Bullet(player2.x, player2.y, player2.angle, True, player2)
 bullet_mouse = Bullet(mouse_x, mouse_y, 0, False)
+width, height, set_res_func, save_config_func = resolution_changer()
 player1_last_bullet_time = 0
 player2_last_bullet_time = 0
 player1_bullet_count = 1
@@ -289,15 +344,15 @@ bullet_cooldown = 3000
 clock = pygame.time.Clock()
 pygame.mixer.set_num_channels(4)
 menu_selection = 0
-options_selection = 0
 powerups = {
     "ammo": {"collected": True, "color": 'goldenrod3'},
     "shield": {"collected": True, "color": 'dodgerblue3'},
 }
-debug = True
+debug = False
 game_active = False
 main_menu = True
 options_menu = False
+options_res_menu = False
 running = True
 
 # Custom events
@@ -313,10 +368,10 @@ while running:
         if event.type == restart_event:
             restart_game()
         if event.type == ammo_event:
-            ammo_x, ammo_y = random.randint(10, 1890), random.randint(10, 990)
+            ammo_x, ammo_y = random.randint(20, (width - 20)), random.randint(20, (height - 20))
             powerups['ammo']['collected'] = False
         if event.type == shield_event:
-            shield_x, shield_y = random.randint(10, 1890), random.randint(10, 990)
+            shield_x, shield_y = random.randint(20, (width - 20)), random.randint(20, (height - 20))
             powerups['shield']['collected'] = False
         if event.type == shield_end_event:
             if player1.health >= 2:
@@ -357,7 +412,6 @@ while running:
         elif main_menu:
             powerups['ammo']['collected'] = True
             powerups['shield']['collected'] = True
-            options_selection = 0
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     menu_selection -= 1
@@ -378,27 +432,59 @@ while running:
                         options_menu = True
                     if menu_selection == 2:
                         running = False
+                    menu_selection = 0
 
         elif options_menu:
-            menu_selection = 0
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    options_selection -= 1
-                    if options_selection <= 0:
-                        options_selection = 0
+                    menu_selection -= 1
+                    if menu_selection <= 0:
+                        menu_selection = 0
                 if event.key == pygame.K_DOWN:
-                    options_selection += 1
-                    if options_selection >= 2:
-                        options_selection = 2
+                    menu_selection += 1
+                    if menu_selection >= 2:
+                        menu_selection = 2
                 if event.key == pygame.K_RETURN:
-                    if options_selection == 2:
+                    if menu_selection == 0:
+                        options_menu = False
+                        options_res_menu = True
+                        menu_selection = 0
+                    if menu_selection == 2:
                         options_menu = False
                         main_menu = True
+                        menu_selection = 0
                 if event.key == pygame.K_ESCAPE:
                     restart_game()
                     options_menu = False
                     main_menu = True
 
+        elif options_res_menu:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    menu_selection -= 1
+                    if menu_selection <= 0:
+                        menu_selection = 0
+                if event.key == pygame.K_DOWN:
+                    menu_selection += 1
+                    if menu_selection >= 3:
+                        menu_selection = 3
+                if event.key == pygame.K_RETURN:
+                    if menu_selection == 0:
+                        set_res_func(1600, 900)
+                    if menu_selection == 1:
+                        set_res_func(1280, 720)
+                    if menu_selection == 2:
+                        set_res_func(854, 480)
+                    if menu_selection == 3:
+                        save_config_func()
+                        options_res_menu = False
+                        options_menu = True
+                        menu_selection = 0
+                if event.key == pygame.K_ESCAPE:
+                    restart_game()
+                    options_menu = False
+                    main_menu = True
+                    menu_selection = 0
 
     if game_active:
         start_ticks = pygame.time.get_ticks()
@@ -414,6 +500,7 @@ while running:
                 player1.img = player1.img_thrust
             else:
                 player1.img = pygame.image.load(f'Graphics/Player_1.png').convert_alpha()
+
         if player1.health <= 0:
             player1.sound_boost_loop.stop()
             player1.explode()
@@ -435,39 +522,39 @@ while running:
         player1.update()
         player2.update()
         screen.fill('black')
-
-        # Centre gravity point
-        pygame.draw.circle(screen, (255, 255, 255), (WIDTH // 2, HEIGHT // 2), 8)
-        pygame.draw.circle(screen, (0, 0, 0), (WIDTH // 2, HEIGHT // 2), 7)
-
+        black_hole(player1)
+        black_hole(player2)
         # Stop drawing when explosion animation ends
         if player1.current_sprite < 9:
             player1.draw()
         if player2.current_sprite < 9:
             player2.draw()
-
         if player1.health >= 2:
             pygame.draw.arc(screen, 'skyblue', ((player1.x - 15), (player1.y - 15), 30, 30), 0, 360, 1)
         if player2.health >= 2:
             pygame.draw.arc(screen, 'skyblue', ((player2.x - 15), (player2.y - 15), 30, 30), 0, 360, 1)
 
         fps = clock.get_fps()
-        player1_bullet.check_bullet_collisions(player2)
-        player1_bullet.check_bullet_collisions(player1)
-        player2_bullet.check_bullet_collisions(player1)
-        player2_bullet.check_bullet_collisions(player2)
-        bullet_mouse.check_bullet_collisions(player1)
-        bullet_mouse.check_bullet_collisions(player2)
+        player1_bullet.modify_bullet_list(player2)
+        player1_bullet.modify_bullet_list(player1)
+        player2_bullet.modify_bullet_list(player1)
+        player2_bullet.modify_bullet_list(player2)
+        bullet_mouse.modify_bullet_list(player1)
+        bullet_mouse.modify_bullet_list(player2)
+
+        if player1.health >= 1 and player2.health >= 1:
+            player1.check_ship_collision(player2)
+            player2.check_ship_collision(player1)
 
         if debug:
-            debug_text('FPS', 10, 10, 24, fps)
-            debug_text('ticks', 40, 10, 24, start_ticks)
-            debug_text('p1_bullets', 70, 10, 24, len(player1_bullet.bullet_list + bullet_mouse.bullet_list))
-            debug_text('p1_angle', 100, 10, 24, player1.angle)
-            debug_text('p1_health', 130, 10, 24, player1.health)
-            debug_text('p2_health', 160, 10, 24, player2.health)
-            debug_text('p1_bullet_count', 190, 10, 24, player1.bullet_count)
-            debug_text('p2_bullet_count', 220, 10, 24, player2.bullet_count)
+            debug_text('FPS', 10, 10, 16, fps)
+            debug_text('ticks', 40, 10, 16, start_ticks)
+            debug_text('p1_bullets', 70, 10, 16, len(player1_bullet.bullet_list + bullet_mouse.bullet_list))
+            debug_text('p1_angle', 100, 10, 16, player1.angle)
+            debug_text('p1_health', 130, 10, 16, player1.health)
+            debug_text('p2_health', 160, 10, 16, player2.health)
+            debug_text('p1_bullet_count', 190, 10, 16, player1.bullet_count)
+            debug_text('p2_bullet_count', 220, 10, 16, player2.bullet_count)
 
         bullets_collide()
         mouse_bullet_spawn()
@@ -483,5 +570,16 @@ while running:
         options_screen()
         pygame.display.update()
 
+    elif options_res_menu == True:
+        resolution_screen()
+        pygame.display.update()
+
     clock.tick(60)
 pygame.quit()
+
+# To do:
+# -Add a new powerup
+
+
+
+
